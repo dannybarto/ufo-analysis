f012706f34107733c8531d96e39a3e2bf799c0bc
dannybarto <85522326+dannybarto@users.noreply.github.com>
2022-03-15 14:26:38 -0400

    dataandfiles

--- Analysis of Unstructured UFO Sightings Data/Deep Learning/inception_v3_ufo/extract.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat inceptionv3_model_part_a* >inceptionv3_model.tar.gz && \
-tar -xzvf inceptionv3_model.tar.gz
--- Analysis of Unstructured UFO Sightings Data/Deep Learning/inception_v4_ufo/extract.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat inceptionv4_model_part_a* >inceptionv4_model.tar.gz && \
-tar -xzvf inceptionv4_model.tar.gz
